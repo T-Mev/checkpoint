518f19dc3c428fecf6aa1c7714821a219a634e45
T-Mev <teddirect@gmail.com>
2020-09-20 22:17:43 -0400

    Added UserRepository

---
+++ src/main/java/com/tmev/checkpoint/models/data/UserRepository.java
@@ -0,0 +1,10 @@
+//package com.tmev.checkpoint.models.data;
+//
+//import com.tmev.checkpoint.models.User;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends CrudRepository<User, Integer> {
+//    User findByUsername(String username);
+//}
