e8f30848f1a12133dad9084f601203b87671bff4
T-Mev <teddirect@gmail.com>
2020-09-20 22:14:23 -0400

    Minor improvements

--- src/main/java/com/tmev/checkpoint/models/dto/UserDTO.java
+++ src/main/java/com/tmev/checkpoint/models/dto/UserDTO.java
@@ -1,5 +1,31 @@
 package com.tmev.checkpoint.models.dto;
 
+import java.util.List;
+
 public class UserDTO {
+
+    private String username;
+    private List<SimpleGameDTO> simpleGames;
+
+    public UserDTO(String username, List<SimpleGameDTO> simpleGames) {
+        this.username = username;
+        this.simpleGames = simpleGames;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<SimpleGameDTO> getSimpleGames() {
+        return simpleGames;
+    }
+
+    public void setSimpleGames(List<SimpleGameDTO> simpleGames) {
+        this.simpleGames = simpleGames;
+    }
 }
 
