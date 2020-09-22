5e1b6cd76b94b233e2be11d09de8861a7d52e642
T-Mev <teddirect@gmail.com>
2020-09-21 23:06:18 -0400

    Added id

--- src/main/java/com/tmev/checkpoint/models/User.java
+++ src/main/java/com/tmev/checkpoint/models/User.java
@@ -6,14 +6,24 @@ import java.util.List;
 //@Entity
 public class User {
 
+    private int id;
     private String username;
     private String password;
     private List<Integer> gamesList;
 
-    public User(String username, String password, List<Integer> gamesList) {
+    public User(int id, String username, String password, List<Integer> gamesList) {
+        this.id = id;
         this.username = username;
         this.password = password;
         this.gamesList = gamesList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -39,6 +49,5 @@ public class User {
     public void setGamesList(List<Integer> gamesList) {
         this.gamesList = gamesList;
     }
-
 }
 
