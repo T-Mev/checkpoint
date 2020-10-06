e493f78e32a53fa12f8f94e24327f73f50bf5e5a
T-Mev <teddirect@gmail.com>
2020-10-05 21:45:37 -0400

    Improved search view

--- src/main/checkpoint-frontend/src/app/components/search/search.component.ts
+++ src/main/checkpoint-frontend/src/app/components/search/search.component.ts
@@ -12,13 +12,14 @@ export class SearchComponent implements OnInit {
 
   //Make this a typescript interface
   games;
-
+  searchTerm: string;
   constructor(private route: ActivatedRoute, private rest: RestService) {
 
   }
 
   ngOnInit() {
     this.route.queryParams.subscribe(param => {
+      this.searchTerm = param.term.toLowerCase().replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase());
       this.rest.getSearch(param.term).subscribe(res => {
         this.games = res;
         console.log(this.games);
