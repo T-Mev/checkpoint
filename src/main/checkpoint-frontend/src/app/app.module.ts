f14951a0b9cf37b2cd38536aad5b2dc35a444289
T-Mev <teddirect@gmail.com>
2020-10-09 17:23:22 -0400

    Refactored Angular Material component implementation

--- src/main/checkpoint-frontend/src/app/app.module.ts
+++ src/main/checkpoint-frontend/src/app/app.module.ts
@@ -10,11 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { UpcomingComponent } from './components/upcoming/upcoming.component';
 import { SearchComponent } from './components/search/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
@@ -31,11 +27,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatToolbarModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
