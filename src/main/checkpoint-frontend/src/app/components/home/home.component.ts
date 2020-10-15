da7a6521da742a494446723443e4e56c5b1f8765
T-Mev <teddirect@gmail.com>
2020-10-14 22:53:14 -0400

    Improved home page, added initial carousel

--- src/main/checkpoint-frontend/src/app/components/home/home.component.ts
+++ src/main/checkpoint-frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RestService } from 'src/app/service/rest.service';
 
 @Component({
@@ -6,16 +6,24 @@ import { RestService } from 'src/app/service/rest.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
+// export class HomeComponent implements OnInit, AfterViewInit {
 
   games;
 
+  // @ViewChild("carouselLoop", { static: false })
+  // carousel: ElementRef;
+
+
   constructor(private rest: RestService) { }
+  // ngAfterViewInit(): void {
+  // }
 
   ngOnInit() {
     this.rest.getPopular().subscribe(res => {
       this.games = res;
       console.log(this.games);
+      // console.log(this.carousel);
     });
   }
 
