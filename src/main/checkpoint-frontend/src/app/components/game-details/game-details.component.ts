2c09414bcc3911145de05f1812dfe5a468ac2a87
T-Mev <teddirect@gmail.com>
2020-10-27 23:16:01 -0400

    Added additional styling and content to game page

--- src/main/checkpoint-frontend/src/app/components/game-details/game-details.component.ts
+++ src/main/checkpoint-frontend/src/app/components/game-details/game-details.component.ts
@@ -22,4 +22,19 @@ export class GameDetailsComponent implements OnInit {
     })
   }
 
+  getReleaseDateRegion(regionID: number): string {
+
+    switch (regionID) {
+      case 1: return "Europe";
+      case 2: return "North America";
+      case 3: return "Australia";
+      case 4: return "New Zealand";
+      case 5: return "Japan";
+      case 6: return "China";
+      case 7: return "Asia";
+      case 8: return "Worldwide";
+    }
+
+  }
+
 }
