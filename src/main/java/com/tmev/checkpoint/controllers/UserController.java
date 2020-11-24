c251d53921b6584a34c964a50cb92dd28b16d267
T-Mev <teddirect@gmail.com>
2020-11-23 22:40:55 -0500

    Refactored UserController's GetMapping

--- src/main/java/com/tmev/checkpoint/controllers/UserController.java
+++ src/main/java/com/tmev/checkpoint/controllers/UserController.java
@@ -1,45 +1,53 @@
-//package com.tmev.checkpoint.controllers;
-//
-//import com.tmev.checkpoint.models.User;
-//import com.tmev.checkpoint.models.data.UserRepository;
-//import com.tmev.checkpoint.models.dto.SimpleGameDTO;
-//import com.tmev.checkpoint.models.dto.UserDTO;
-//import com.tmev.checkpoint.services.ApiData;
-//import org.json.JSONException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/REST/user/")
-//public class UserController {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @GetMapping("{id}")
-//    public UserDTO user (@PathVariable int id) throws InterruptedException, JSONException, IOException {
-//
-//        User user = userRepository.findById(id).orElseThrow();
-//        List<SimpleGameDTO> simpleGames = new ArrayList<>();
-//
-//        for (int gameId : user.getGamesList()) {
-//        simpleGames.add(ApiData.getSimpleGame(gameId));
-//        }
-//        return new UserDTO(user.getUsername(), simpleGames);
-//    }
-//}
+package com.tmev.checkpoint.controllers;
 
-// grab user
-// get their list of game Ids
-// morph that into a comma separated string
-// pass that into the API call
-// send API data and User data to front end with the use of UserCollectionDTO
+import com.api.igdb.apicalypse.APICalypse;
+import com.api.igdb.exceptions.RequestException;
+import com.api.igdb.request.IGDBWrapper;
+import com.api.igdb.request.JsonRequestKt;
+
+import com.tmev.checkpoint.models.User;
+import com.tmev.checkpoint.models.data.UserRepository;
+import com.tmev.checkpoint.services.ApiService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/REST/user/")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ApiService apiService;
+
+    // Handles requests at /REST/user/
+    @GetMapping("{userId}")
+    public String displayUserCollection (@PathVariable int userId) throws RequestException {
+
+        // Setting User and getting games list
+        User user = userRepository.getById(userId);
+        String gamesList = user.getGamesList().stream().map(String::valueOf).collect(Collectors.joining(","));
+
+        // Authenticating requests for the IGDB API
+        IGDBWrapper wrapper = IGDBWrapper.INSTANCE;
+        wrapper.setCredentials(apiService.getClientId(), apiService.getAccessToken());
+
+        try{
+            return JsonRequestKt.jsonGames(IGDBWrapper.INSTANCE, new APICalypse()
+                    .fields("name, cover.image_id")
+                    .where("id = (" + gamesList + ")"));
+        } catch(RequestException e) {
+            System.out.println(e.getStatusCode());
+            throw e;
+        }
+    }
+}
 
 // create @PostMapping to handle edits to User's game collection
--- src/main/java/com/tmev/checkpoint/models/data/UserRepository.java
+++ src/main/java/com/tmev/checkpoint/models/data/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-    User findById(long id);
+    User getById(long id);
 }
