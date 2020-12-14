3bab817435c126b70a341ecdf8454f807d258ded
T-Mev <teddirect@gmail.com>
2020-12-14 00:02:32 -0500

    Created services for UserDetails

--- src/main/java/com/tmev/checkpoint/services/UserDetailsService.java
+++ src/main/java/com/tmev/checkpoint/services/UserDetailsService.java
@@ -1,2 +1,8 @@
-package com.tmev.checkpoint.services;public interface UserDetailsService {
+package com.tmev.checkpoint.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
