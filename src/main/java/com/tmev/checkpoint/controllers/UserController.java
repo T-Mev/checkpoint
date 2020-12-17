51176708847aede0c829e89c3e7c1d2e32281ad5
T-Mev <teddirect@gmail.com>
2020-12-16 23:49:21 -0500

    Improved displayUserCollection logic

--- src/main/java/com/tmev/checkpoint/controllers/UserController.java
+++ src/main/java/com/tmev/checkpoint/controllers/UserController.java
@@ -1,59 +1,60 @@
-//package com.tmev.checkpoint.controllers;
-//
-//import com.api.igdb.apicalypse.APICalypse;
-//import com.api.igdb.exceptions.RequestException;
-//import com.api.igdb.request.IGDBWrapper;
-//import com.api.igdb.request.JsonRequestKt;
-//
-//import com.tmev.checkpoint.models.User;
-//import com.tmev.checkpoint.models.data.UserRepository;
-//import com.tmev.checkpoint.services.ApiService;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Optional;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//@RequestMapping("/REST/user")
-//public class UserController {
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private ApiService apiService;
-//
-//    // Change id to username
-//    // Handles GET requests at /REST/user?id=
-//    @GetMapping
-//    public String displayUserCollection (@RequestParam Long id) throws RequestException {
-//
-//        // Setting User and getting games list
-//        Optional<User> user = userRepository.findById(id);
-//        if (user.isPresent()) {
-//            String gamesList = user.get().getGamesList().stream().map(String::valueOf).collect(Collectors.joining(","));
-//        } else {
-////            throw
-////            custom error handling when user is not found
-//        }
-//
-//
-//        // Authenticating requests for the IGDB API
-//        IGDBWrapper wrapper = IGDBWrapper.INSTANCE;
-//        wrapper.setCredentials(apiService.getClientId(), apiService.getAccessToken());
-//
-//        // IGDB API Call
-//        try{
-//            return JsonRequestKt.jsonGames(IGDBWrapper.INSTANCE, new APICalypse()
-//                    .fields("name, cover.image_id")
-//                    .where("id = (" + gamesList + ")"));
-//        } catch(RequestException e) {
-//            System.out.println(e.getStatusCode());
-//            throw e;
-//        }
-//    }
+package com.tmev.checkpoint.controllers;
+
+import com.api.igdb.apicalypse.APICalypse;
+import com.api.igdb.exceptions.RequestException;
+import com.api.igdb.request.IGDBWrapper;
+import com.api.igdb.request.JsonRequestKt;
+
+import com.tmev.checkpoint.models.User;
+import com.tmev.checkpoint.models.data.UserRepository;
+import com.tmev.checkpoint.services.ApiService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/REST/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ApiService apiService;
+
+    // Handles GET requests at /REST/user?name=
+    @GetMapping
+    public String displayUserCollection (@RequestParam String name) throws RequestException {
+
+        // Authenticating requests for the IGDB API
+        IGDBWrapper wrapper = IGDBWrapper.INSTANCE;
+        wrapper.setCredentials(apiService.getClientId(), apiService.getAccessToken());
+
+        // Setting User and getting games list
+        Optional<User> user = userRepository.findByUsername(name);
+
+        if (user.isPresent()) {
+            String gamesList = user.get().getGamesList().stream().map(String::valueOf).collect(Collectors.joining(","));
+
+            // IGDB API Call
+            try{
+                return JsonRequestKt.jsonGames(IGDBWrapper.INSTANCE, new APICalypse()
+                        .fields("name, cover.image_id")
+                        .where("id = (" + gamesList + ")"));
+            } catch(RequestException e) {
+                System.out.println(e.getStatusCode());
+                throw e;
+            }
+
+        } else {
+//            throw custom error handling when user is not found
+            return "User not found";
+        }
+
+    }
 //
 //    // Handles POST requests at /REST/user?id=&gameId=
 //    @PostMapping
@@ -86,5 +87,5 @@
 //        User user = userRepository.getById(id);
 //        return user.containsGame(gameId);
 //    }
-//
-//}
+
+}
