cedb4d8afc39bcd0d7c622b2bf2a0bf866c23e6c
T-Mev <teddirect@gmail.com>
2020-12-17 00:13:35 -0500

    Refactored login / register forms

--- src/main/checkpoint-frontend/src/app/components/form-login/form-login.component.ts
+++ src/main/checkpoint-frontend/src/app/components/form-login/form-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { TokenStorageService } from 'src/app/service/token-storage.service';
 
 @Component({
   selector: 'app-form-login',
@@ -6,13 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent implements OnInit {
+  form: any = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  currentUser: any;
 
-  constructor() { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.currentUser = this.tokenStorage.getUser();
+      console.log(this.currentUser.username);
+    }
   }
 
-  // onSubmit() {
-  // }
+  onSubmit() {
+    this.authService.login(this.form).subscribe(
+      res => {
+        this.tokenStorage.saveToken(res.accessToken);
+        this.tokenStorage.saveUser(res);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        setTimeout(() => this.reloadPage(), 1000)
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    );
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
 
 }
